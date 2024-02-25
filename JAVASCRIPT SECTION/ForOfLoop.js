b038f1d439562717512c5f9076f3a4d5c9b57d9b
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-02-25 11:45:10 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/ForOfLoop.js
@@ -0,0 +1,4 @@
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+for(fruit of fruits) {
+    console.log(fruit);
+}
---
+++ JAVASCRIPT SECTION/NestedForOfArray.js
@@ -0,0 +1,7 @@
+let heroes = [["superman", "batman", "wonder woman"], ["spiderman", "ironman", "thor"]];
+for (list of heroes) {
+    console.log(list);
+}
+for(hero of list) {
+    console.log(hero);
+}
