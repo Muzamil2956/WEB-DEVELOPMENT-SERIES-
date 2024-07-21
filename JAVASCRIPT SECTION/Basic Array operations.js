e99d1690775b8771b2d931f2de90d535af8c07bb
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-21 13:18:49 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/Basic Array operations.js
@@ -0,0 +1,12 @@
+const fruits = ['apple', 'orange', 'Mango']
+console.log(fruits);
+fruits.push('Banana'); // add at last
+console.log(fruits);
+fruits.unshift('strawberry'); // add at first
+console.log(fruits);
+fruits.pop(); //delete last one
+console.log(Array.isArray('strawberry'));
+console.log(fruits);
+console.log(fruits.indexOf('orange'));
+
+
