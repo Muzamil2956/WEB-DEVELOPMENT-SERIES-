1bfa33890588d0b480ced2f763dfca74993cc20e
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-22 13:00:50 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/Object literals operations.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: 'Will',
+    lastName: 'Smith',
+    Age: 23,
+    hobbies: ['cricket', 'chess'],
+    Adress: {
+        City: 'Madrid'
+    }
+}
+person.email = 'will@gmail.com'
+console.log(person);
+console.log(person.lastName);
+console.log(person.lastName, person.firstName);
+console.log(person.hobbies[1]);
