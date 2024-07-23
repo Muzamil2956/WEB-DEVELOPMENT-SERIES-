1262e37888d6045a00d966d2bd601e768ca3c19d
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-23 14:44:16 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/ForEach Method.js
@@ -0,0 +1,9 @@
+//The forEach() method calls a function and iterates over the elements of an array
+
+let students = ['John', 'Sara', 'Jack'];
+students.forEach(myFunction);
+function myFunction(item) {
+    console.log(item);
+}
+   
+         
