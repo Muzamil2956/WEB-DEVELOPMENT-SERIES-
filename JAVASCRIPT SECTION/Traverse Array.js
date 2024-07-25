336b02fb8f10d4e3d71b027251efb827e5ed2229
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-25 11:32:31 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/Traverse Array.js
@@ -0,0 +1,9 @@
+function loopArray() {
+    let arr = ['Item 1', 'Item 2', 'Item 3',
+        'Item 4', 'Item 5'];
+
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}                           
+loopArray();
