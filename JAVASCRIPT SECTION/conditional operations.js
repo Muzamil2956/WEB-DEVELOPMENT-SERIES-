e7ce313f06bffd80cf9afd5b2f9f54c40f7e0f21
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-28 10:33:48 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/conditional operations.js
@@ -0,0 +1,12 @@
+const x = 12;
+if (x===10){
+  console.log('x is 10');
+}
+else if(x % 2==0){
+  console.log('x is even');
+}
+else if (x>10){
+  console.log('x is greater than 10');
+}
+else {
+  console.log('x is less than 10')};
