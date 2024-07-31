7ccca8c5379b91fd905a470a30364b6d8c669078
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-07-31 12:56:52 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/switch format.js
@@ -0,0 +1,15 @@
+const color ='green';
+switch(color) {
+    case 'red':
+        console.log('Dangeour');
+        break;
+        case 'yellow':
+         console.log('warning');
+         break;
+             case 'green':
+             console.log('everthig good');
+             break;
+                default:
+                console.log('color not match');
+                break;
+}
