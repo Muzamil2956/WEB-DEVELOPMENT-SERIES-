3aae730acdf8b5a159fdebc62f49c38038f506e2
Muzamil Majeed <90823440+Muzamil2956@users.noreply.github.com>
2024-08-05 10:49:58 +0530

    Add files via upload

---
+++ JAVASCRIPT SECTION/constructor.js
@@ -0,0 +1,13 @@
+// Define a constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Create instances of Person using the constructor
+const person1 = new Person("Alice", 30);
+const person2 = new Person("Bob", 25);
+
+// Access properties of the instances
+console.log(person1.name); 
+console.log(person2.age);  
